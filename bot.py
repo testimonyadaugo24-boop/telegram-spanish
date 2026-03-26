6fff78cc6ecf108fae83d69cceaa50d0032d00ba
ferdibobo <ferdinandoguchukwu@gmail.com>
2026-03-26 09:31:54 -0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -9,23 +9,40 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID"))
 
 async def translate_post(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.channel_post and update.channel_post.chat.id == CHANNEL_ID:
-        if update.channel_post.text:
-            original_text = update.channel_post.text
+        # Get text from either plain text or caption
+        original_text = update.channel_post.text or update.channel_post.caption
 
+        if original_text:
             # Translate to Spanish
             translated = GoogleTranslator(source='auto', target='es').translate(original_text)
 
-            # Delete original English message
+            # Delete original message
             await context.bot.delete_message(
                 chat_id=CHANNEL_ID,
                 message_id=update.channel_post.message_id
             )
 
-            # Send Indonesian version
-            await context.bot.send_message(
-                chat_id=CHANNEL_ID,
-                text=translated
-            )
+            # Send translated message
+            if update.channel_post.photo:
+                # If it was a photo, resend photo with translated caption
+                await context.bot.send_photo(
+                    chat_id=CHANNEL_ID,
+                    photo=update.channel_post.photo[-1].file_id,  # highest resolution
+                    caption=translated
+                )
+            elif update.channel_post.document:
+                # If it was a document, resend document with translated caption
+                await context.bot.send_document(
+                    chat_id=CHANNEL_ID,
+                    document=update.channel_post.document.file_id,
+                    caption=translated
+                )
+            else:
+                # Otherwise, just send text
+                await context.bot.send_message(
+                    chat_id=CHANNEL_ID,
+                    text=translated
+                )
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(MessageHandler(filters.ALL, translate_post))
